ff0f5eb9c30d65b5003750f01ec60caa4d3761fd
vladimir-siv <vladimir.sivcev@gmail.com>
2020-02-07 20:49:49 +0100

    CreatorController & Terrain Boundary

---
+++ Assets/Scripts/CreatorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CreatorController : MonoBehaviour
+{
+	[SerializeField]
+	private float Speed = 1.0f;
+
+	[SerializeField]
+	private float RotationalSpeed = 1.0f;
+
+	private void Update()
+	{
+		var direct = 0.0f;
+		var strafe = 0.0f;
+		var fly = 0.0f;
+
+		if (Input.GetKey(KeyCode.W)) direct += 1.0f;
+		if (Input.GetKey(KeyCode.S)) direct -= 1.0f;
+		if (Input.GetKey(KeyCode.A)) strafe -= 1.0f;
+		if (Input.GetKey(KeyCode.D)) strafe += 1.0f;
+		if (Input.GetKey(KeyCode.Mouse3)) fly -= 1.0f;
+		if (Input.GetKey(KeyCode.Mouse4)) fly += 1.0f;
+
+		if (Input.GetMouseButton(1))
+		{
+			var rotSpeed = RotationalSpeed * Time.deltaTime;
+			transform.Rotate(-Input.GetAxis("Mouse Y") * Vector3.right * rotSpeed);
+			transform.Rotate(Input.GetAxis("Mouse X") * Vector3.up * rotSpeed, Space.World);
+		}
+
+		transform.position += (transform.forward * direct + transform.right * strafe + transform.up * fly).normalized * Speed * Time.deltaTime;
+	}
+}
