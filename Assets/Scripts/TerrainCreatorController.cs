745aa2cb558f0e3731c7abaf9cbf0ef1e95b117b
vladimir-siv <vladimir.sivcev@gmail.com>
2020-02-11 20:22:22 +0100

    Brush display

--- Assets/Scripts/TerrainCreatorController.cs
+++ Assets/Scripts/TerrainCreatorController.cs
@@ -6,16 +6,26 @@ using Terrain = TerrainGenerator.Terrain;
 public class TerrainCreatorController : MonoBehaviour
 {
 	[SerializeField] private float Granularity = 0.1f;
-	[SerializeField] private float BrushRadius = 1.0f;
 
-	private BoxCollider Collider { get; set; }
+	private BoxCollider TerrainCollider { get; set; }
 
 	private Terrain ObservedTerrain { get; set; }
 	private float[] OutputValues { get; set; }
 
+	private GameObject Brush { get; set; }
+	private Material BrushMaterial { get; set; }
+	private float BrushRadius { get; set; } = 1.0f;
+	private float BrushZCorretion { get; set; } = 0.0f;
+
 	private void Start()
 	{
-		Collider = GetComponent<BoxCollider>();
+		Brush = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Brush.name = "CursorSphere";
+		Destroy(Brush.GetComponent<SphereCollider>());
+		BrushMaterial = Brush.GetComponent<MeshRenderer>().material;
+		BrushMaterial.shader = Shader.Find("Unlit/ColorBlend");
+
+		TerrainCollider = GetComponent<BoxCollider>();
 		ObservedTerrain = new Terrain();
 
 		// Create dynamically
@@ -37,28 +47,39 @@ public class TerrainCreatorController : MonoBehaviour
 
 	private void Update()
 	{
-		var build = Input.GetMouseButton(0);
-		var clear = Input.GetMouseButton(1);
+		var mouse = Input.mousePosition;
+		var cam = Camera.main;
 
-		/*
-		if (build ^ clear)
+		var z = Vector3.Distance(cam.transform.position, transform.position);
+		var center = cam.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, z + BrushZCorretion));
+		var alpha = 0.0f;
+
+		if (TerrainCollider.bounds.Contains(center))
 		{
-			var center = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			alpha = 0.3f;
 
-			if (Collider.bounds.Contains(center))
+			if (Input.GetKey(KeyCode.UpArrow)) BrushRadius += 0.01f;
+			if (Input.GetKey(KeyCode.DownArrow)) BrushRadius -= 0.01f;
+			BrushZCorretion += Input.mouseScrollDelta.y / 10.0f;
+
+			if (Input.GetKeyDown(KeyCode.R))
 			{
-				Debug.Log(center);
+				BrushRadius = 1.0f;
+				BrushZCorretion = 0.0f;
+			}
+
+			var build = Input.GetMouseButton(0);
+			var clear = Input.GetMouseButton(1);
+
+			if (build ^ clear)
+			{
+				//ObservedTerrain.Calculate(OutputValues);
 			}
 		}
-		*/
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			ObservedTerrain.Calculate(OutputValues);
-
-			for (var i = 0; i < OutputValues.Length; ++i)
-				Debug.Log(OutputValues[i]);
-		}
+		Brush.transform.position = center;
+		Brush.transform.localScale = new Vector3(BrushRadius, BrushRadius, BrushRadius);
+		BrushMaterial.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f, alpha));
 	}
 
 	private void OnDestroy()
---
+++ Assets/Shaders/ColorBlend.shader
@@ -0,0 +1,37 @@
+﻿Shader "Unlit/ColorBlend"
+{
+	Properties
+	{
+		_Color("_Color", COLOR) = (1,1,1,1)
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha
+			
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			fixed4 _Color;
+			
+			float4 vert(float4 position: POSITION): SV_POSITION
+			{
+				return UnityObjectToClipPos(position);
+			}
+			
+			fixed4 frag(float4 position: SV_POSITION): SV_Target
+			{
+				return _Color;
+			}
+			
+			ENDCG
+		}
+	}
+}
